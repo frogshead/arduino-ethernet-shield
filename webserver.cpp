d67d095f2164b465bbe93383698a6e7ecdc8f47d
Mikko Viitamäki <mikko.viitamaki@gmail.com>
2013-08-27 11:20:31 +0300

    Make ethernet shield's ip address to match to ip address space

--- webserver.cpp
+++ webserver.cpp
@@ -22,7 +22,7 @@
 // The IP address will be dependent on your local network:
 byte mac[] = { 
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192,168,1,177);
+IPAddress ip(192,168,100,177);
 
 // Initialize the Ethernet server library
 // with the IP address and port you want to use 
